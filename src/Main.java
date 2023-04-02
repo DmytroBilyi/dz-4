c115705357c34484a5b9a8ac5271be58182ae350
dbeliy@outlook.com <dbeliy@outlook.com>
2023-04-02 22:14:16 +0200

    dz-4

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         Second second = new Second();
-        String s = String.format("~~~%s~~~", second.printMethod());
-        System.out.println(s);
+        String o = String.format("~~~%s~~~", second.printMethod());
+        System.out.println(o);
 
     }
 }
